f255b10247a3422a791d58c562fff4ed958d2758
Shubhranshu2057 <studansh9@gmail.com>
2026-06-06 13:53:07 +0530

    First base push

---
+++ src/start.java
@@ -0,0 +1,16 @@
+public class start {
+    public static void main(String[] args){
+        String Messages = """
+        --------Welcome TO Smart Banking Management System------
+        1) Login Account
+        2) Create Account
+        3) Help
+          """;
+        System.out.println(Messages);
+        int choise = 0;
+        System.out.println();
+        switch (choise){
+        }
+
+    }
+}
